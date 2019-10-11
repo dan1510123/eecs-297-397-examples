27888e002e6547bbd1e1b2a3896996c6b81de431
Matthew Sargent <matthew.sargent@case.edu>
2019-10-10 23:09:33 -0400

    Fix indentation. Add test cases

--- assignment-003/assignment3.py
+++ assignment-003/assignment3.py
@@ -1,6 +1,6 @@
 # Implement this function:
 def store_checkout(inventory_tuple_list, item_purchase_list):
-"""
+    """
     inventory_tuple_list: 
         A list of tuples. Each tuple has a string representing an
         item name, an int representing a price, and a string representing
@@ -22,13 +22,13 @@ def store_checkout(inventory_tuple_list, item_purchase_list):
 
     The example inputs here would have a total cost of:
     5 + 5 + 10 + 0 = 20 
-"""
+    """
     return 0
 
 
 # Implement this function:
 def highest_frequency_count(item_list):
-"""
+    """
     item_list:
         A list of strings. Each string represents an item name.
 
@@ -40,5 +40,5 @@ def highest_frequency_count(item_list):
 
     In the example, "A" and "B" each appear 3 times, while "C" appears 
     only 1 time. Therefore, expected return value would be 3.
-"""
+    """
     return 0
